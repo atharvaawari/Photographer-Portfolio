3ec6953e08a6f5d4993ec5cbb8c3df049951888c
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2024-02-12 15:26:24 +0530

    created components and routings for pages

---
+++ vivekPhotography/src/components/index.js
@@ -0,0 +1,7 @@
+import About from './About'
+import Book from './Book'
+import Contact from './Contact'
+import Home from './Home'
+import Layout from './Layout'
+
+export default {About, Book, Contact, Home, Layout } 
--- vivekPhotography/src/main.jsx
+++ vivekPhotography/src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements,  } from 'react-router-dom'
+import {Layout, Home, About, Contact, Book} from './components/index.js'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='bookusnow' element={<Book/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
